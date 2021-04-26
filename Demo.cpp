89e59a078bd6cad00c76a9eea13a2faa05009c6b
ortiram <ortiran5@gmail.com>
2021-04-26 11:34:12 +0300

    numWhitUnit-b

---
+++ Demo.cpp
@@ -0,0 +1,11 @@
+#include <map>
+using namespace std;
+using namespace ariel;
+
+
+int main(){
+     map<string,map<string, double>> units;
+     units["a"]["b"] = 4;
+     cout >> units;
+    return 0[]
+}
